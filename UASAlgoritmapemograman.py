ffe72f238de0a2e0baab179252231540c0d22992
Hadi Anwar <76906181+Hadihachi@users.noreply.github.com>
2021-01-26 09:07:55 +0800

    Commit

--- UASAlgoritmapemograman.py
+++ UASAlgoritmapemograman.py
@@ -1,12 +1,15 @@
+
 print("Program menghitung waktu,jarak dan kecepatan")
-print("======================================")
+print("============================================")
+budi = int (input("masukkan kecepatan : "))
+yandi = int (input("masukkan kecepatan : "))
 #Diketahui
 #pukul berangkat Budi = 05.00
 #pukul berangkat Yandi = 05.15
 #kecepatan Budi = 60km/jam
 #kecepatan Yandi = 40km/jam
 #jarak jakarta dan bandung = 150km
-kecepatan = 60 + 40 
+kecepatan = budi + yandi
 jarak_total = 150
 waktu_berpapasan = jarak_total / kecepatan
 print ( waktu_berpapasan , " = 01.30")
